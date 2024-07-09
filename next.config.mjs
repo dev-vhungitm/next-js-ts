3a01753c7024b763a57e8b8fd8dd61f3bf67e9b7
dev-vhungitm <dev-vhungitm@gmail.com>
2024-07-09 20:21:48 +0700

    Update variables in styles

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,13 @@ const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'scss'), path.join(__dirname, 'app')],
-    prependData: `@import "styles/variables.scss";`
+    includePaths: [path.join(__dirname, 'scss'), path.join(__dirname, 'components')],
+    prependData: `
+      @import "styles/variables.scss";
+      @import "styles/variables.module.scss";
+      @import "styles/mixins.scss";
+      @import "styles/functions.scss";
+    `
   }
 };
 
